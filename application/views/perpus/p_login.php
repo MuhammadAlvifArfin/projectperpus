67fd5d9d9289d19927c03dcfe9217c62ef6e37a1
MuhammadAlvifArfin <muhammadalvifarfin@gmail.com>
2020-04-24 14:46:53 +0700

    mengganti view pada halaman login

--- application/views/perpus/p_login.php
+++ application/views/perpus/p_login.php
@@ -1,45 +1,56 @@
-      
+<div class="login-box">
+  <div class="login-logo">
+    <a href="#"><b>THE</b>Perpustakaan</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
 
-
-
-        <!-- Sing in  Form -->
-        <section class="sign-in">
-            <div class="container">
-                <div class="signin-content">
-                    <div class="signin-image">
-                        <figure><img src="<?= base_url('regform/'); ?>images/signin-image.jpg" alt="sing up image"></figure>
-                        <a href="<?= base_url('Login/registrasi') ?>" class="signup-image-link">Create an account</a>
-                    </div>
-
-                    <div class="signin-form">
-                        <h2 class="form-title">Sign up</h2>
-                        <form class="register-form" id="login-form" action="<?php echo site_url('login/dologin') ?>" method="post">
-                            <div class="form-group">
-                                <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="username" placeholder="Username" autocomplete="off" required/>
-                                
-                            </div>
-                            <div class="form-group">
-                                <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="password" placeholder="Password" required/>
-                            </div>
-                            <div>
-                                <center>
-                                        <?php
-                                        if ($this->session->flashdata('error')!==null)
-                                        {
-                                        ?>
-                                            <?php echo $this->session->flashdata('error') ?>
-                                        <?php
-                                        }
-                                        ?>
-                                </center>
-                            </div>
-                            <div class="form-group form-button">
-                                <input type="submit" class="form-submit">
-                            </div>
-                        </form>
-                    </div>
-                </div>
+      <form action="<?php echo site_url('login/dologin') ?>" method="post">
+        <div class="input-group mb-3">
+          <input type="text" name="username" class="form-control" placeholder="Username" required autocomplete="off">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
             </div>
-        </section>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" name="password" class="form-control" placeholder="Password" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8 ">
+              <small class="text-danger">
+                <?php
+                if ($this->session->flashdata('error')!==null)
+                {
+                ?>
+                <?php echo $this->session->flashdata('error') ?>
+                <?php
+                    }
+                ?>
+              </small>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      
+      <p class="mb-0">
+        <a href="<?= base_url('Login/registrasi') ?>" class="text-center">Register a new membership</a>
+      </p>
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
